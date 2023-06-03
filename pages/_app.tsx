e02571ae8d463c8a7d6b99888348dbf3a83e2987
FunamiYuiii <mossbirdgreen@gmail.com>
2023-06-03 22:44:43 +0900

    土台完成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,44 @@
 // pages/_app.js
-import { Box, ChakraProvider, VStack } from '@chakra-ui/react'
+import { Box, ChakraProvider, Container, HStack, VStack } from '@chakra-ui/react'
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Menu from '../components/menu';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet" /> 
         <title>
-          久留米ラーメンブログ
+          🍜久留米ラーメンブログ
         </title>
       </Head>
-      <Box w='full' minH='100vh' p='relative'>
-        <Header />
-        <VStack>
-          <Component {...pageProps} /> 
-        </VStack>
-        <Box p='absolute' bottom='0'>
-          <Footer />
-        </Box>        
-      </Box>      
+      <Box w='full' minH='100vh' fontFamily='Yuji Syuku'>
+        <Container display='flex' maxW='1000px'>
+          <HStack w='full'>
+            <Box pos='relative' display={{ base: 'none', md: 'block' }}>
+              <Box pos='fixed' top='100px'>
+                <Menu />
+              </Box>
+            </Box>
+            <Container w={{ base: '325px', md: '500px' }}>
+              <VStack w='full' spacing='50px' align='start' mt={{ base: '10px', md: '0px' }}>
+                <Header />
+                <Box display={{ base: 'block', md: 'none' }}>
+                  <Menu />
+                </Box>
+                <Component {...pageProps} />
+                <Footer />
+              </VStack>
+            </Container>
+          </HStack>
+        </Container>
+      </Box>
     </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import { Box } from "@chakra-ui/react"
+import { Box, VStack, Text } from "@chakra-ui/react"
+import Shop from "../components/shop"
 
 const IndexPage = () => {
     return (
-        <Box>
-            ラーメン
+        <Box w='full'>
+            <VStack spacing={10} >
+                <Box w={{ base: '325px', md: '500px'}}>
+                    <Text fontSize={{ base: '14px', md: '16px'}}>
+                        久留米高専在学中の生徒が久留米市にあるラーメン店を食べ歩き、書いたブログと
+                        なっています。
+                    </Text>
+                </Box>
+                <Shop id='mns' name='久留米ラーメン道 麺志 本店' path='/img/mns_GTO1.jpeg'
+                    des='西鉄久留米駅から徒歩3分の位置にあるラーメン店です。GTOスペシャル、'
+                    address='久留米市東町39-2馬場ビル1階' />
+                <Shop id='th' name='大砲ラーメン 本店' path='/img/th_G1.jpeg'
+                    des=''
+                    address='久留米市通外町11-8' />
+                <Shop id='dr' name='大龍ラーメン 小森野店' path='/img/dr_G1.jpeg'
+                    des=''
+                    address='佐賀県鳥栖市真木町1056-1' />
+                <Shop id='m1' name='満一 ゴールドレーベル フレスタ久留米店' path='/img/m1.jpeg'
+                    des=''
+                    address='久留米市城南町2-34 フレスタ久留米 2F' />
+            </VStack>
         </Box>
     )
 }
