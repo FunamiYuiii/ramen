ada03dd2fa7e773e0754b3184051fbb3f443e5e7
FunamiYuiii <mossbirdgreen@gmail.com>
2023-06-03 23:04:51 +0900

    build

---
+++ out/_next/static/CSjjbnWlNfcXHHpUepq4M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b2508e398f9e6087.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
