0ef6124a95bd28155e2187230347e9a562af2746
FunamiYuiii <mossbirdgreen@gmail.com>
2023-06-03 16:15:15 +0900

    add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 // pages/_app.js
 import { Box, ChakraProvider, VStack } from '@chakra-ui/react'
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>
+          久留米ラーメンブログ
+        </title>
+      </Head>
       <Box w='full' h='100vh'>
         <VStack>
           <Component {...pageProps} /> 
